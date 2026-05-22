06555d1bc9f5de3f86797657e0ce369cad839a54
Himanshi <rajeshdost6@gmail.com>
2026-05-22 20:02:51 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,10 @@ const payload = {
 
 const askQuestion=async()=>{
 
+  if(!question){
+    return false;
+  }
+
   if(localStorage.getItem('history')){
     let history = JSON.parse(localStorage.getItem('history'))
     history = [question,...history]
@@ -23,7 +27,7 @@ const askQuestion=async()=>{
     setRecentHistory(history)
   }
    else{
-    localStorage.setItem('history',JSON.stringif([question]))
+    localStorage.setItem('history',JSON.stringify([question]))
     setRecentHistory([question])
    } 
 
@@ -53,16 +57,35 @@ const askQuestion=async()=>{
       )
   
   setResult([...result,{type:'q',text:question},{type:'a',text:dataString}]);
-  //setResult(response.candidates[0].content.parts[0].text);
+  setQuestion('')
+}
+
+const clearHistory =()=>{
+  localStorage.clear();
+  setRecentHistory([]);
+}
+
+const isEnter =(e)=>{
+  console.log(e.key);
+if(e.key =="Enter"){
+  askQuestion();
+}
+
 }
 
   return (
     <div className='grid grid-cols-5 h-screen  bg-black'>
      <div className='col-span-1 bg-black text-white p-4 shadow-2xl border-zinc-800 border'>
-      <ul>
-        {
-          recentHistory && recentHistory.map((item)=>(
-            <li>
+      <h1 className='flex justify-center '>
+         <span className='pr-2'>
+        Recent Search
+        </span>
+     <button onClick={clearHistory} className='cursor-pointer'><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z"/></svg></button>
+     </h1>
+      <ul className='overflow-auto text-sm'>
+        { recentHistory && recentHistory.map((item,index)=>(
+          <li key={index} 
+          className='truncate w-55 p-1 cursor-pointer hover:bg-zinc-500 overflow-hidden'>
               {item}
             </li>
           ))
@@ -102,7 +125,7 @@ const askQuestion=async()=>{
      </div>
       <div className=' fixed bottom-5 left-[60%] -translate-x-1/2  w-2/4 text-white bg-zinc-800 m-auto 
       rounded-4xl  flex items-center h-14 outline-none '>
-        <input type='text' value={question}  onChange={(event)=>setQuestion(event.target.value)}  placeholder='Ask anything' className='p-4 mx-3 flex-2 outline-none'/>
+        <input onKeyDown={isEnter} type='text' value={question}  onChange={(event)=>setQuestion(event.target.value)}  placeholder='Ask anything' className='p-4 mx-3 flex-2 outline-none'/>
         <button onClick={askQuestion} className='cursor-pointer bg-zinc-800 rounded-full h-10 w-10 text-white text-2xl flex justify-center items-center'>↑</button>
       </div>
      </div>
